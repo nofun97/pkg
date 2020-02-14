7444b13487f5745e1ab7f30c40e8acab9d82a82c
Novan Allanadi <25704935+nofun97@users.noreply.github.com>
2020-02-14 15:40:54 +1100

    added comments

--- log/grpchelper/injector.go
+++ log/grpchelper/injector.go
@@ -7,6 +7,9 @@ import (
 	"google.golang.org/grpc"
 )
 
+// BuildLoggerInjectorMiddleware returns a middleware that injects logger to the grpc context.
+// It accepts fields so that you can add fields and configurations. If fields has no logger,
+// a standard logger will be injected.
 func BuildLoggerInjectorMiddleware(fieldsWithLogger log.Fields) grpc.UnaryServerInterceptor {
 	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (resp interface{}, err error) {
 		if !fieldsWithLogger.HasLogger() {
@@ -18,6 +21,7 @@ func BuildLoggerInjectorMiddleware(fieldsWithLogger log.Fields) grpc.UnaryServer
 	}
 }
 
+// InfoMiddleware returns a middleware that does a non-verbose log.
 func InfoMiddleware(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (resp interface{}, err error) {
 	resp, err = handler(ctx, req)
 	log.Infof(ctx, "Info: %s", info.FullMethod)
